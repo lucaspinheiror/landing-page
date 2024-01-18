c07e2d26a75e6ccbc7dfaf70313707ef001615d0
lucaspinheiror <pinheiroi338@gmail.com>
2024-01-18 16:15:35 -0300

    Terminando projeto adaptado para desktop

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function menuAbrir(){
 
     menu.classList.toggle('abrir')
 } 
+
+
+function fecharMenuMobile() {
+    const larguraTela = window.innerWidth;
+
+    const pontoDeRuptura = 1439;
+    
+    if (larguraTela > pontoDeRuptura) {
+        var menu = document.getElementById('navigation');
+        
+        if (menu) {
+            menu.classList.remove('abrir');
+        }
+    }
+}
+
+window.addEventListener('resize', fecharMenuMobile);
